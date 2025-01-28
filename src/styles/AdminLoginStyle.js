b4828127a04a75d25e68c9844f5d8c414e19f1b6
chldsbdud <https://github.com/chldsbdud>
2025-01-28 14:29:50 +0000

    ✨ "[Feat]
    합격자 조회"

--- src/styles/AdminLoginStyle.js
+++ src/styles/AdminLoginStyle.js
@@ -1,4 +1,57 @@
 import styled from "styled-components";
 import palette from "@lib/colorPalette";
 
-export const AdminLogin = styled.div``;
+export const AdminLogin = styled.div`
+  height: 25px;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const InputBox = styled.div`
+  width: 342px;
+  height: 48px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: start;
+  border-radius: 24px;
+  background-color: white;
+  color: #d9650d;
+  margin: 8px;
+`;
+
+export const InputName = styled.div`
+  width: 90px;
+  font-size: 16px;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Input = styled.input`
+  outline: none;
+  border: none;
+`;
+
+export const Bar = styled.div`
+  width: 1px;
+  height: 30px;
+  background-color: #d9650d;
+  position: relative;
+  right: 10px;
+`;
+
+export const Button = styled.button`
+  width: 342px;
+  height: 48px;
+  border-radius: 24px;
+  background-color: #ff7710;
+  color: white;
+  border: none;
+  margin: 10px;
+  font-size: 18px;
+`;
--- src/styles/ApplicantsResultStyle.js
+++ src/styles/ApplicantsResultStyle.js
@@ -1,4 +1,70 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import palette from "@lib/colorPalette";
 
-export const ApplicantsResult = styled.div``;
+export const ApplicantsResult = styled.div`
+  height: 50px;
+`;
+
+export const Page = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Box = styled.div`
+  background-color: white;
+  width: 302px;
+  padding: 20px;
+  color: black;
+  border-radius: 24px;
+`;
+
+export const Info = styled.div`
+  font-size: 18px;
+  font-weight: 400;
+`;
+
+export const H1 = styled.div`
+  font-size: 28px;
+  font-weight: 700;
+`;
+
+export const H2 = styled.div`
+  font-size: 20px;
+  font-weight: 500;
+`;
+
+export const Orange = styled.div`
+  color: #ff7710;
+  font-size: 18px;
+  font-weight: 600;
+`;
+
+export const Img = styled.img`
+  width: 20px;
+  height: 20px;
+  margin-bottom: 3px;
+`;
+
+export const LinkBox = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const StyledLink = styled(Link)`
+  color: #78726e;
+`;
+
+export const Button = styled.button`
+  height: 20px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  gap: 5px;
+
+  border: none;
+  background-color: white;
+`;
